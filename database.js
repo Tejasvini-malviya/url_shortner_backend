1d06c71094065724ffb030dafbd2d2b8f0004827
Tejasvini Malviya <tejasvinimalviya@gmail.com>
2026-01-13 11:03:23 +0530

    using database url

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT, // postgres
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres", // ** required **
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
